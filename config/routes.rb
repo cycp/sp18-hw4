c0dc768eec066f301efe62248acf664208b27243
Clara Park <cycp@berkeley.edu>
2018-03-07 14:43:56 -0800

    idgi

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
-  post 'todos/new', to: 'todos#create'
+  get 'todos/new', to: 'todos#new'
+  post 'todos', to: 'todos#create'
 end
